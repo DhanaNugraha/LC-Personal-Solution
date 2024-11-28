5e43cef92a5a153e4c0a50d5cfd8b64d4ef40bda
DhanaNugraha <dhananugraha1511@gmail.com>
2024-11-28 20:18:33 +0700

    15, even skipping values are still slow

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -2,40 +2,50 @@
 # save as output given in example
 # for in for in for in range full, full -1, full -2
 # time limit exceeded
+# try sorting and skipping same numbers for i first then j, then k
 
 
-nums = [-1,0,1,2,-1,-4]
+nums = [-1,0,1,2,-1,-4,-1]
 
 def threeSum(nums):
+    # sort the nums for easier skip later
+    nums = sorted(nums)
+    print(sorted(nums))
+
     # initial location of pointers
-    left = 0
-    right = len(nums) - 1
+    i = 0
+    j = 1
+    k = 2
 
     # stores output in list
     output = []
 
-    # traverse nums as two pointers
-    for left in range(0, len(nums) - 1):
-        for right in range(left + 1, len(nums)):
+    # traverse through the nums list as three pointers
+    for i in range(0, len(nums) - 2):
+        # skip the number if it was previously tested as i
+        if i > 0 and nums[i] == nums[i - 1]:
+            print(nums[i], nums[i-1], 'i')
+            continue
 
-            # find in nums for final value to result sum 0. pop to ensure we do not check for same index.
-            leftValue = nums.pop(left)
-            rightValue = nums.pop(right - 1)
+        for j in range(i + 1, len(nums) - 1):
+            # skip the number if it was previously tested as j
+            if j > (i + 1) and nums[j] == nums[j - 1]:
+                print(nums[j], nums[j-1], 'j')
+                continue
 
-            # last value = 0 - i - j
-            find = - leftValue - rightValue
+            for k in range((j + 1), len(nums)):
+                # skip the number if it was previously tested as k
+                if k > (j + 1) and nums[k] == nums[k - 1]:
+                    print(nums[k], nums[k-1], 'k')
+                    continue
+                print(i, j, k)
+                currentSum = nums[i] + nums[j] + nums[k]
 
-            if find in nums:
-                # for checking if it already exist in output
-                check = sorted([leftValue, rightValue, find])
-
-                if check not in output:
-                    output.append(check)
-
-            # insert values back to nums
-            nums.insert(left, leftValue)
-            nums.insert(right, rightValue)
-
+                if currentSum == 0:
+                    check = sorted((nums[i], nums[j], nums[k]))
+                    if check not in output:
+                        output.append(check)
+                
     return output
 
 print(threeSum(nums))
