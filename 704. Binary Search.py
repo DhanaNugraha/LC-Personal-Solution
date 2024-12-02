f8d608eb2ced4e503d9688f4f425acee3063c6f9
DhanaNugraha <dhananugraha1511@gmail.com>
2024-12-02 19:59:09 +0700

    704, 0ms, 13.09MB: proper binary search using left right pointers

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -2,10 +2,25 @@ nums = [-1,0,3,5,9,12]
 target = 9
 
 def search(nums, target):
-    for i, num in enumerate(nums):
-        if num == target:
-            return i
+    left, right = 0, len(nums) - 1
+    
+    # nums can be 1
+    while left <= right:
+        # int division to floor
+        mid = (left + right) // 2
+        curNum = nums[mid]
+
+        if curNum == target:
+            return mid
+
+        elif curNum < target:
+            left = mid + 1
+        
+        else:
+            right = mid - 1
+            
     return -1
+
 
 
 print(search(nums, target))
