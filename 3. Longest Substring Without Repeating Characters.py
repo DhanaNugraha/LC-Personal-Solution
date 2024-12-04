79aaae4ba9a4012405936b81bbe5e7b3df35e1c3
DhanaNugraha <dhananugraha1511@gmail.com>
2024-12-04 14:25:15 +0700

    3, 19ms, 12.97MB: Real sliding window

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,27 +1,17 @@
 s = "aab"
 
 def lengthOfLongestSubstring(s):
-    left, right = 0, 0
+    left = 0
     substring = set()
     output = 0
 
-    while right < len(s):
-        print('passed here')
-        if s[right] not in substring:
-            print(s[right], 'im here')
-            substring.add(s[right])
-        
-        else:
-            print(output, substring)
-            output = max(output, len(substring))
+    for right in range(len(s)):
+        while s[right] in substring:
+            substring.remove(s[left])
             left += 1
-            right = left
-            substring = set()
-            continue
-
-        right += 1
-
-    output = max(output, len(substring))
+        
+        substring.add(s[right])
+        output = max(output, len(substring))
 
     return output
 
