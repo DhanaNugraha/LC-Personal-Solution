b9f90789304f7e1c6051047c862f6dfe7a2faa20
DhanaNugraha <dhananugraha1511@gmail.com>
2024-12-10 18:03:00 +0700

    100: added simpler solution

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -33,3 +33,14 @@ class Solution(object):
         dfs(p, q)
 
         return self.output
+
+# simpler solution
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val!=q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
