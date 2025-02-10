ac0beee56c07b59bc04d82b47b7803e872442d12
DhanaNugraha <dhananugraha1511@gmail.com>
2025-02-10 19:28:08 +0700

    3110, 0ms, 12.28MB: starting with 0 to 1 before final is faster than 1 to final in list

--- 3110. Score of a String.py
+++ 3110. Score of a String.py
@@ -3,8 +3,8 @@ s = "zaz"
 
 def scoreOfString(s):
     score = 0
-    for i in range(1, len(s)):
-        score  += abs(ord(s[i]) - ord(s[i-1]))
+    for i in range(0, len(s) - 1):
+        score  += abs(ord(s[i]) - ord(s[i+1]))
     return score
 
 print (scoreOfString(s))
